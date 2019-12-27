b170bb62b82e702025098f699878a2607fea9d45
Damien Goujard <damien@macuser.fr>
2019-12-27 11:51:19 +0100

    Ajout dockerfile pour le dashboard + modif pipeline

--- dasboarduptime/config/prod.env.js
+++ dasboarduptime/config/prod.env.js
@@ -1,7 +1,10 @@
 'use strict'
+let urlApiServer = process.env.api_uptime_url || "http://127.0.0.1:3000/";
+if(urlApiServer.slice(-1) != "/")
+  urlApiServer += "/";
 module.exports = {
   NODE_ENV: '"production"',
-  urlAPI:'"https://apiuptime.swarm.actigraph.com/"',
+  urlAPI:'"'+urlApiServer+'"',
   Accounts: 
   [{name:'"info@actigraph.com"', key:['"5d39a641a7bc6b309069b467"']}, 
   {name:'"externe@actigraph.com"', key:['"5d39a658a7bc6b309069b468"']}
